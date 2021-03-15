199ed205bfb2024a66e3f9e3627de75a38a38efe
Sergey <beltsins89@gmail.com>
2021-03-15 19:59:04 +0300

    fix

--- Store/Store.Identity/Models/Response/UserManagerResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Store.Identity.Models.Response
-{
-    public class UserManagerResponse
-    {
-        public string Message { get; set; }
-        public bool IsSuccess { get; set; }
-        public UserResponse User { get; set; }
-    }
-}
